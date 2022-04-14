1228efbc8feace7aa1cf72d42ef7aa8d41d4a8fd
rocketpie <mail.l.conrad@gmail.com>
2022-04-15 00:42:20 +0200

    idea

---
+++ HttpClient.cs
@@ -0,0 +1,16 @@
+﻿using C4Http.Default;
+
+namespace C4Http
+{
+    public class HttpClient
+    {
+        private RequestWriter _requestWriter;
+        private ResponseReader _responseReader;
+
+        public HttpClient()
+        {
+            _requestWriter = new Default.RequestWriter();
+            _responseReader = new Default.ResponseReader();
+        }
+    }
+}
